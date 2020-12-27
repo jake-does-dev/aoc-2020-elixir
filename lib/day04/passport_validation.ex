dc4784abe46208a81a4522d53b51f8ec2413ae00
Jake Allan <jjemail1995@gmail.com>
2020-12-27 12:35:09 +0000

    day04 part2

--- lib/day04/passport_validation.ex
+++ lib/day04/passport_validation.ex
@@ -1,28 +1,64 @@
 defmodule PassportValidation do
   @required_fields_list ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-  @spec num_valid_passports(any) :: non_neg_integer
-  def num_valid_passports(passports) do
+  def num_valid_passports(passports, is_check_validity) do
     passports
     |> Enum.map(&String.replace(&1, "\n", " "))
-    |> Enum.filter(&valid_passport?(&1, MapSet.new(@required_fields_list)))
+    |> Enum.filter(&valid_passport?(&1, is_check_validity, MapSet.new(@required_fields_list)))
     |> Enum.count()
   end
 
-  @spec extract_keys(any) :: any
-  def extract_keys(passport) do
-    Enum.reduce(passport, MapSet.new(), fn e, acc ->
-      [key, _] = String.split(e, ":")
-      MapSet.put(acc, key)
-    end)
-  end
-
-  @spec valid_passport?(String.t(), MapSet.t(String.t())) :: boolean
-  def valid_passport?(passport, required_fields) do
+  def valid_passport?(passport, is_check_validity, required_fields) do
     without_cid =
-      extract_keys(String.split(passport, " "))
+      passport
+      |> String.split(" ")
+      |> extract_keys(is_check_validity)
       |> MapSet.delete("cid")
 
     MapSet.subset?(required_fields, without_cid)
+  end
+
+  @spec extract_keys(any) :: any
+  def extract_keys(passport_data, is_check_validity \\ false)
+  def extract_keys(passport_data, false), do: extract_keys_no_validity_check(passport_data)
+  def extract_keys(passport_data, true), do: extract_keys_with_validity_check(passport_data)
+
+  defp extract_keys_no_validity_check(passport_data) do
+    Enum.reduce(passport_data, MapSet.new(), fn entry, keys ->
+      [key, _] = String.split(entry, ":")
+      MapSet.put(keys, key)
+    end)
+  end
+
+  defp extract_keys_with_validity_check(passport_data) do
+    Enum.reduce(passport_data, MapSet.new(), fn entry, keys ->
+      [key, val] = String.split(entry, ":")
+      if valid?(key, val) do
+        MapSet.put(keys, key)
+      else
+        keys
+      end
+    end)
+  end
+
+  defp valid?("byr", val), do: between?(val, 1920, 2002)
+  defp valid?("iyr", val), do: between?(val, 2010, 2020)
+  defp valid?("eyr", val), do: between?(val, 2020, 2030)
+  defp valid?("hcl", val), do: String.match?(val, ~r/#[A-Fa-f0-9]{6}$/)
+  defp valid?("ecl", val), do: MapSet.member?(MapSet.new(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]), val)
+  defp valid?("pid", val), do: String.match?(val, ~r/^\d{9}$/)
+  defp valid?("hgt", val) do
+    case Regex.run(~r/(\d+)(in|cm)/, val) do
+      [_, amount, "cm"] -> between?(amount, 150, 193)
+      [_, amount, "in"] -> between?(amount, 59, 76)
+      nil -> false
+    end
+  end
+  defp valid?("cid", _), do: true
+  defp valid?(_, _), do: false
+
+  defp between?(val, lower, upper) do
+    as_int = String.to_integer(val)
+    lower <= as_int && as_int <= upper
   end
 end
--- test/day04/passport_validation_test.exs
+++ test/day04/passport_validation_test.exs
@@ -3,21 +3,21 @@ defmodule PassportValidationTest do
 
   test "small case required fields" do
     passports = FileReader.read_lines("test/day04/smallCase.txt", "\n\n")
-    assert 2 == PassportValidation.num_valid_passports(passports)
+    assert 2 == PassportValidation.num_valid_passports(passports, false)
   end
 
   test "puzzle input required fields" do
     passports = FileReader.read_lines("test/day04/puzzleInput.txt", "\n\n")
-    assert 242 == PassportValidation.num_valid_passports(passports)
+    assert 242 == PassportValidation.num_valid_passports(passports, false)
   end
 
-  # test "small case field validation" do
-  #   passports = FileReader.read_lines("test/day04/smallCase.txt", "\n\n")
-  #   assert 2 == PassportValidation.num_valid_passports(passports)
-  # end
+  test "small case field validation" do
+    passports = FileReader.read_lines("test/day04/smallCase.txt", "\n\n")
+    assert 2 == PassportValidation.num_valid_passports(passports, true)
+  end
 
-  # test "puzzle input field validation" do
-  #   passports = FileReader.read_lines("test/day04/smallCase.txt", "\n\n")
-  #   assert 242 == PassportValidation.num_valid_passports(passports)
-  # end
+  test "puzzle input field validation" do
+    passports = FileReader.read_lines("test/day04/puzzleInput.txt", "\n\n")
+    assert 0 == PassportValidation.num_valid_passports(passports, true)
+  end
 end
